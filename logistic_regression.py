5964d78b06ba06337a46b0232250f50d71f71d76
Abdul Samad <81066980+Abdulsamad27@users.noreply.github.com>
2023-05-27 20:48:40 +0530

    Update and rename logistic_regression.ipynb to logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,4 +1,3 @@
-
         import numpy as np
         import matplotlib.pyplot as plt
         import pandas as pd
@@ -6,8 +5,7 @@
         dataset = pd.read_csv('Social_Network_Ads.csv')
         X = dataset.iloc[:, :-1].values
         y = dataset.iloc[:, -1].values
-      
-      
+            
         ## Splitting the dataset into the Training set and Test set
             
         from sklearn.model_selection import train_test_split
